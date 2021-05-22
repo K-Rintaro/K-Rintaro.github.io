3968801e324d5c77bcff6d164141590e762fb8d9
K-Rintaro <k.rintaro1@icloud.com>
2021-05-22 19:24:00 +0900

    hahahahaha

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.TOKEN || 3000;
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(port, () => {
     console.log(`this app listeming on port ${port}`)
 })
